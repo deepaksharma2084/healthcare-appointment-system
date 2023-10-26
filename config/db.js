b538f65f87792bad09571a7694d7c22ba6f4c1de
Deepak sharma <115777684+deepaksharma2084@users.noreply.github.com>
2023-10-26 09:05:48 +0530

    chnage1
    change1

--- config/db.js
+++ config/db.js
@@ -3,7 +3,6 @@ const connectDB = async()=>{
     try {
          await mongoose.connect(process.env.DB_URL);
 
-         console.log(`Mongodb connected ${mongoose.connection.host}`);
         
     } catch (error) {
         console.log(error);
